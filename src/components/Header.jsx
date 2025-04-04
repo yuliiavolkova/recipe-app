ce223db34369bacf54b25401d2efa474044e434b
yuliia volkova <yuliia_volkova@icloud.com>
2025-04-04 18:59:11 +0200

    Started from scratch and created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="/src/images/globe.png" alt="globe icon" className="logo" />
+      <h1>React RECIPE project</h1>
+    </header>
+  );
+}
