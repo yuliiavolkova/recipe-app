a4c1906491e7a2d64fd2a172a06f40db4212442a
yuliia volkova <yuliia_volkova@icloud.com>
2025-04-07 19:49:47 +0200

    added ingredient form and dynamic ingredient list to Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,32 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    //console.log("Ingredient added");
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    console.log(newIngredient);
+    ingredients.push(newIngredient); // Add the new ingredient to the ingredients array
+    console.log(ingredients);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
