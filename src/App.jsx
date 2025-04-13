d69d337d65a02a50c21eec5ab5143fe27a90dc2f
yuliia volkova <yuliia_volkova@icloud.com>
2025-04-13 12:56:27 +0200

    returned back to chef claude

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-/*import Header from "./components/Header";
+import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
@@ -8,8 +8,8 @@ export default function App() {
       <Main />
     </div>
   );
-}*/
-
+}
+/*
 import React from "react"; // Import React
 export default function App() {
   const [isGoingOut, setIsGoingOut] = React.useState(false);
@@ -31,11 +31,34 @@ export default function App() {
       </button>
     </main>
   );
-}
-
+}*/
 //const isGoingOut = true;
 /*if (isGoingOut) {
     answer = "Yes, let's go out!";
   } else {
     answer = "No, let's stay in.";
   }*/
+
+/*import React from "react";
+export default function App() {
+  const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
+  const allFavoriteThings = ["apple", "banana", "orange", "grape", "kiwi"];
+
+  const thingsElements = myFavoriteThings.map((thing) => (
+    <p key={thing}>{thing}</p>
+  ));
+
+  function addFavoriteThings() {
+    setMyFavoriteThings((prevFavThings) => [
+      ...prevFavThings,
+      allFavoriteThings[prevFavThings.length],
+    ]);
+  }
+
+  return (
+    <main>
+      <button onClick={addFavoriteThings}>Add things</button>
+      <section aria-live="polite">{thingsElements}</section>
+    </main>
+  );
+}*/
