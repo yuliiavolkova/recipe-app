495cd306a4b0f29094c6d2c9568f69e714aee65e
yuliia volkova <yuliia_volkova@icloud.com>
2025-04-13 13:10:21 +0200

    updated chef claude, refactor array state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
+import React from "react";
 export default function Main() {
-  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const [ingredients, setIngredients] = React.useState([]);
 
   const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
@@ -10,8 +11,10 @@ export default function Main() {
     //console.log("Ingredient added");
     const formData = new FormData(event.currentTarget);
     const newIngredient = formData.get("ingredient");
+
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
     console.log(newIngredient);
-    ingredients.push(newIngredient); // Add the new ingredient to the ingredients array
+    //ingredients.push(newIngredient); // Add the new ingredient to the ingredients array
     console.log(ingredients);
   }
 
