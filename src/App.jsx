ca1ae8523b751e2010bfbee89597ba8c12a014c0
yuliia volkova <yuliia_volkova@icloud.com>
2025-04-13 12:12:05 +0200

    created function which changes answer (yes or no) with button onclick

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,26 @@ export default function App() {
   );
 }*/
 
+import React from "react"; // Import React
 export default function App() {
-  const isGoingOut = true;
+  const [isGoingOut, setIsGoingOut] = React.useState(false);
 
-  /*if (isGoingOut) {
+  function changeMind() {
+    setIsGoingOut((prev) => !prev);
+  }
+  return (
+    <main>
+      <h1 className="title">Do I feel like going out tonight?</h1>
+      <button onClick={changeMind} className="value">
+        {isGoingOut ? "Yes" : "No"}
+      </button>
+    </main>
+  );
+}
+
+//const isGoingOut = true;
+/*if (isGoingOut) {
     answer = "Yes, let's go out!";
   } else {
     answer = "No, let's stay in.";
   }*/
-  return (
-    <main>
-      <h1 className="title">Do I feel like going out tonight?</h1>
-      <button className="value">{isGoingOut ? "Yes" : "No"}</button>
-    </main>
-  );
-}
